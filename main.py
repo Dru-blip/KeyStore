f5513f6542c873e555c2805368f0c08532b6ea43
Druva kumar Reddy Biduduri <62353412+Dru-blip@users.noreply.github.com>
2026-01-23 16:24:46 +0530

    implemented simple home/vault UI screens/widgets using stacked widget to replace them on button click.

--- main.py
+++ main.py
@@ -4,7 +4,41 @@ from PySide6.QtWidgets import (
     QApplication,
     QLabel,
     QMainWindow,
+    QPushButton,
+    QStackedWidget,
+    QVBoxLayout,
+    QWidget,
 )
+
+
+class HomeWidget(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.label = QLabel("Welcome to KeyStore!")
+        self.open_button = QPushButton("Open")
+        self.open_button.clicked.connect(self.open_vault)
+        self.vlayout = QVBoxLayout()
+        self.vlayout.addWidget(self.label)
+        self.vlayout.addWidget(self.open_button)
+        self.setLayout(self.vlayout)
+
+    def open_vault(self):
+        self.parent().setCurrentIndex(1)
+
+
+class VaultWidget(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.label = QLabel("Welcome to Vault!")
+        self.go_back_button = QPushButton("Go Back")
+        self.go_back_button.clicked.connect(self.go_back)
+        self.vlayout = QVBoxLayout()
+        self.vlayout.addWidget(self.label)
+        self.vlayout.addWidget(self.go_back_button)
+        self.setLayout(self.vlayout)
+
+    def go_back(self):
+        self.parent().setCurrentIndex(0)
 
 
 class MainWindow(QMainWindow):
@@ -13,6 +47,11 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("My App")
 
         self.setup_menubar()
+        self.container = QStackedWidget()
+        self.container.addWidget(HomeWidget())
+        self.container.addWidget(VaultWidget())
+
+        self.setCentralWidget(self.container)
 
     def setup_menubar(self):
         menubar = self.menuBar()
@@ -25,11 +64,10 @@ class MainWindow(QMainWindow):
         new_vault_action.triggered.connect(self.new_vault)
 
         file_menu.addAction(new_vault_action)
-        exit_action=file_menu.addAction("Exit")
+        exit_action = file_menu.addAction("Exit")
         exit_action.setIcon(QIcon.fromTheme("application-exit"))
         exit_action.triggered.connect(self.close)
 
-    
     def new_vault(self):
         pass
 
